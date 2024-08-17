875b80b540f5eea10b09a2cd2999d1ea7cdd2b3a
admin <mahatobir22@gmail.com>
2024-08-17 21:19:05 +0530

    This is first for today after editing the code

--- src/main/java/com/dailycodebuffer/springbootex/springbootdemo/controller/CarManagementSystemController.java
+++ src/main/java/com/dailycodebuffer/springbootex/springbootdemo/controller/CarManagementSystemController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CarManagementSystemController {
     @GetMapping("/msg")
     public String getMessage(){
-        return "Tis is first APP";
+        return "This is first APP for Car Rental System";
     }
 }
